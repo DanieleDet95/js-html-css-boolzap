30e177968b96ab61774bd2ea19db35bbb0918db8
Daniele <danieledetommaso1995@gmail.com>
2020-06-16 17:52:23 +0200

    Aggiunta invio messaggio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,55 @@
 $(document).ready(
   function(){
 
-    
+    // Al click da tastiera invio,invia messaggio
+    $('.bottom input').keydown(
+      function(event){
+
+        if($('.bottom input').val().length > 0){
+          if(event.which == 13){
+            aggiungiMessaggioUtente();
+
+            setTimeout(function(){
+              aggiungiMessaggioComputer();
+            }, 3000);
+
+          }
+        }
+      }
+    );
+
+    // Al click del bottone,invia messaggio
+    $('.bottom i').click(
+      function(){
+
+        if($('.bottom input').val().length > 0){
+          aggiungiMessaggioUtente();
+
+          setTimeout(function(){
+            aggiungiMessaggioComputer();
+          }, 3000);
+        }
+
+      }
+    );
 
 });
+
+// Funzione di inserimento messaggio utente
+function aggiungiMessaggioUtente(){
+  var bloccoMessaggio = $('.template .myMessage').clone();
+  bloccoMessaggio.removeClass('nascosto');
+  var messaggio = $('.bottom input').val();
+  bloccoMessaggio.prepend(messaggio);
+  $('.wrapper').append(bloccoMessaggio);
+  $('.bottom input').val('');
+}
+
+// Funzione di inserimento messaggio computer
+function aggiungiMessaggioComputer(){
+  var bloccoMessaggio = $('.template .otherMessage').clone();
+  bloccoMessaggio.removeClass('nascosto');
+  var messaggio = 'Ok';
+  bloccoMessaggio.prepend(messaggio);
+  $('.wrapper').append(bloccoMessaggio);
+}
